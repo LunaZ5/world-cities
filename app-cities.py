9c7c59f375eafdec3a4fa09c868494eaebf12f30
LunaZ5 <lunazhang56@gmail.com>
2025-08-12 03:10:44 -0700

    Update app-cities.py

--- app-cities.py
+++ app-cities.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_theme()
+# import seaborn as sns
+# sns.set_theme()
 
 
 st.title('World Cites')
